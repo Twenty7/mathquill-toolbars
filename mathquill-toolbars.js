71e0558c7b652ccf676a48ed3f8c05046d002a2a
Matt Taylor <mtaylor@illuminateed.com>
2014-03-29 07:40:03 -0700

    Add 'inline' toolbar style. Rename classes.

--- mathquill-toolbars.js
+++ mathquill-toolbars.js
@@ -11,6 +11,7 @@ $.widget( "mathquill.mathquillToolbars", {
 		times: { label: '\\times', latex: '\\times', description: 'Multiplication', cmd: 'cmd' },
 		divide: { label: '\\divide', latex: '\\divide', description: 'Division', cmd: 'cmd' },
 		equal: { label: '=', latex: '=', description: 'Equal', cmd: 'cmd' },
+		pm: { label: '\\pm', latex: '\\pm', description: 'Plus-Minus', cmd: 'cmd' },
 		lt: { label: '<', latex: '<', description: 'Less Than', cmd: 'cmd' },
 		gt: { label: '>', latex: '>', description: 'Greater Than', cmd: 'cmd' },
 		lte: { label: '\\le', latex: '\le', description: 'Less Than or Equal To', cmd: 'cmd' },
@@ -18,8 +19,8 @@ $.widget( "mathquill.mathquillToolbars", {
 		frac: { label: '\\frac {x}{y}', latex: '\\frac', description: 'Fraction', cmd: 'cmd' },
 		parens: { label: '(x)', latex: '(', description: 'Parentheses', cmd: 'cmd' },
 		brackets: { label: '[x]', latex: '[', description: 'Brackets', cmd: 'cmd' },
-		exponent: { label: '2^{1}', latex: '^{1}', description: 'Exponent', cmd: 'write' },
-		subscript: { label: '2_{1}', latex: '_{1}', description: 'Subscript', cmd: 'write' },
+		exponent: { label: 'x^{2}', latex: '^{2}', description: 'Exponent', cmd: 'write' },
+		subscript: { label: 'x_{2}', latex: '_{2}', description: 'Subscript', cmd: 'write' },
 		absolute: { label: '|x|', latex: '|', description: 'Absolute Value', cmd: 'cmd' },
 		pi: { label: '\\pi', latex: '\\pi', description: 'Pi', cmd: 'write' },
 		sqrt: { label: '\\sqrt x', latex: '\\sqrt', description: 'Square Root', cmd: 'cmd' },
@@ -39,11 +40,12 @@ $.widget( "mathquill.mathquillToolbars", {
 	},
 
 	options: {
+		style: 'buttons',
 		record_timer_delay: 1000,
 		num_undos: 5,
 		buttons: {},
-		toolbars: ['operators', 'structure', 'symbols', 'trig', 'variables'],
-		operators: ['plus', 'minus', 'times', 'divide', 'equal', 'lt', 'gt', 'lte', 'gte'],
+		default_toolbars: ['operators', 'structure', 'symbols', 'trig', 'variables'],
+		operators: ['plus', 'minus', 'times', 'divide', 'equal', 'pm', 'lt', 'gt', 'lte', 'gte'],
 		structure: ['frac', 'parens', 'brackets', 'exponent', 'subscript', 'absolute'],
 		symbols: ['pi', 'sqrt', 'sqrt2', 'imaginary', 'degrees', 'theta', 'phi'],
 		trig: ['sin', 'cos', 'tan', 'arcsin', 'arccos', 'arctan'],
@@ -52,10 +54,29 @@ $.widget( "mathquill.mathquillToolbars", {
 
 	_create: function() {
 		// Init from hidden
-		this.mq_span = $('.mathquill-display .mathquill-editable', this.element);
-		this.mq_input = $('.mathquill-input input', this.element);
+		mq_display = $('.mathquill-toolbars-display', this.element);
+		if (mq_display.length == 0) {
+			console.error("Mathquill Toolbars: '.mathquill-toolbars-display' not found.");
+		}
+		this.mq_span = $('.mathquill-editable', mq_display);
+
+		// Init Mq
+		if (this.mq_span.length != 0) {
+			this.mq_span.mathquill('editable');
+		}
+
+		// Style
+		if (this.element.hasClass('mathquill-toolbars-inline')) {
+			this.options.style = 'dropdown';
+		}
+
+		// Fetch 'hidden' input
+		this.mq_input = $('.mathquill-toolbars-input input', this.element);
+		if (this.mq_input.length == 0) {
+			console.error("Mathquill Toolbars: '.mathquill-toolbars-input input' not found.");
+		}
 		var latex = this.mq_input.val();
-		if (latex.length !== 0) {
+		if (latex && latex.length !== 0) {
 			this.mq_span.mathquill('write', latex);
 			this.saved_equations.push(latex);
 		}
@@ -63,21 +84,25 @@ $.widget( "mathquill.mathquillToolbars", {
 			this.saved_equations.push('');
 		}
 
+		// Toolbars & Events
 		this._buildToolbars();
 		this._bindEvents();
 	},
 
 	_buildToolbars: function() {
-		var toolbar_html = '';
+		if (this.options.toolbars == undefined) {
+			this.options.toolbars = this.options.default_toolbars;
+		}
+		var toolbar_groups = [];
 		if (this.options.toolbars.length != 0) {
-			for (var i = 0; i <= this.options.toolbars.length - 1; i++) {
-				var toolbar_name = this.options.toolbars[i];
+			for (var g = 0; g <= this.options.toolbars.length - 1; g++) {
+				toolbar_groups[g] = [];
+				var toolbar_name = this.options.toolbars[g];
 				if (this.options[toolbar_name] == undefined) {
 					console.error("Mathquill Toolbars: Toolbar '" + toolbar_name + "' not found.");
 					continue;
 				}
 				var toolbar = this.options[toolbar_name];
-				toolbar_html += '<div class="mathquill-toolbar btn-group">';
 				for (var x = 0; x <= toolbar.length - 1; x++) {
 					var btn_name = toolbar[x];
 					var btn_opt;
@@ -90,13 +115,48 @@ $.widget( "mathquill.mathquillToolbars", {
 					else {
 						console.error("Mathquill Toolbars: Button '" + btn_name + "' not found.");
 					}
-					toolbar_html += '<button class="btn btn-small" data-fragment="' + btn_opt.name + '" data-latex="' + btn_opt.latex + '" data-cmd="' + btn_opt.cmd + '" title="' + btn_opt.description + '"><span class="mathquill-embedded-latex">' + btn_opt.label + '</span></button>';
+					// Button HTML
+					if (this.options.style == 'dropdown') {
+						toolbar_groups[g].push('<a href="javascript://" class="mathquill-toolbars-btn" data-fragment="' + btn_opt.name + '" data-latex="' + btn_opt.latex + '" data-cmd="' + btn_opt.cmd + '" title="' + btn_opt.description + '"><span class="mathquill-embedded-latex">' + btn_opt.label + '</span></a>');
+					}
+					else {
+						toolbar_groups[g].push('<button class="mathquill-toolbars-btn btn btn-small" data-fragment="' + btn_opt.name + '" data-latex="' + btn_opt.latex + '" data-cmd="' + btn_opt.cmd + '" title="' + btn_opt.description + '"><span class="mathquill-embedded-latex">' + btn_opt.label + '</span></button>');
+					}
+
 				};
-				toolbar_html += '</div>';
 			};
+			if (this.options.style == 'dropdown') {
+				var toolbar_html = '<span class="mathquill-toolbarset">';
+				toolbar_html += '<div class="mathquill-toolbars btn-group">';
+				toolbar_html += '<a href="javascript://" class="btn dropdown-toggle" data-toggle="dropdown"><i class="icon-superscript"></i><span class="caret"></span></a>';
+				var width = toolbar_groups.length * 80;
+				toolbar_html += '<ul class="dropdown-menu" style="width: ' + width + 'px;">';
+				for (var g = 0; g <= toolbar_groups.length - 1; g++) {
+					toolbar_html += '<li><ul class="mathquill-toolbars-list">';
+					for (var b = 0; b <= toolbar_groups[g].length - 1; b++) {
+						toolbar_html += '<li>' + toolbar_groups[g][b] + '</li>';
+					}
+					toolbar_html += '</ul></li>';
+				}
+				toolbar_html += '</ul></div></span>';
+				$(this.element).append(toolbar_html);
+			}
+			else {
+				var toolbar_html = '<div class="mathquill-toolbarset">';
+				for (var g = 0; g <= toolbar_groups.length - 1; g++) {
+					toolbar_html += '<div class="mathquill-toolbars btn-group">';
+					for (var b = 0; b <= toolbar_groups[g].length - 1; b++) {
+						toolbar_html += toolbar_groups[g][b];
+					}
+					toolbar_html += '</div>';
+				}
+				toolbar_html += '</div>';
+				$(this.element).prepend(toolbar_html);
+
+			}
 		}
-		this.element.prepend(toolbar_html);
-		$('.mathquill-toolbar .mathquill-embedded-latex', this.element).mathquill();
+
+		$('.mathquill-toolbars .mathquill-embedded-latex', this.element).mathquill();
 	},
 
 	_bindEvents: function() {
@@ -105,22 +165,32 @@ $.widget( "mathquill.mathquillToolbars", {
 			$(this).mathquillToolbars('save');
 		});
 
+		// Dropdown Menu Click
+		$(".mathquill-toolbarset a.dropdown-toggle", this.element).on( "click", function() {
+			var dropdown = $(this).parent();
+			// Dirty Hack: no event on bootstrap dropdown ?
+			window.setTimeout(function() {
+				$('.dropdown-menu .mathquill-embedded-latex', dropdown).mathquill('redraw');
+			}, 50);
+		});
+
 		// Toolbar clicks
-		$(".mathquill-toolbar .btn", this.element).on( "click", function() {
+		$(".mathquill-toolbars .mathquill-toolbars-btn", this.element).on( "click", function() {
 			var btn = $(this);
-			btn.parents('.mathquill-container').mathquillToolbars('insert', btn);
+			btn.parents('.mathquill-toolbars-container').mathquillToolbars('insert', btn);
+			return false;
 		});
 
 		// MQ Keydown / Keypress
 		this.mq_span.on( "keydown", function() {
-			$(this).parents('.mathquill-container').mathquillToolbars('setSaveTimer');
+			$(this).parents('.mathquill-toolbars-container').mathquillToolbars('setSaveTimer');
 		}).on( "keypress", function() {
-			$(this).parents('.mathquill-container').mathquillToolbars('setSaveTimer');
+			$(this).parents('.mathquill-toolbars-container').mathquillToolbars('setSaveTimer');
 		});
 	},
 
 	insert: function(btn) {
-		var editor = $('.mathquill-editable').focus();
+		var editor = $('.mathquill-editable', $(btn).parents('.mathquill-toolbars-container')).focus();
 		var latex = btn.data('latex');
 		if (latex) {
 			editor.mathquill(btn.data('cmd'), latex).focus();
@@ -130,11 +200,11 @@ $.widget( "mathquill.mathquillToolbars", {
 
 	setSaveTimer: function() {
 		if (this.saveTimer() !== undefined) return true;
-		
+
 		function callSave(obj) {
 			obj.element.trigger("save");
 		}
-		var timeout = setTimeout(callSave.bind(this, this), this.options.record_timer_delay);
+		var timeout = window.setTimeout(callSave.bind(this, this), this.options.record_timer_delay);
 		this.saveTimer(timeout);
 	},
 
